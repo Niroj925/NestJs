d2b92d81955a83382d51e25d9a44819cfb6bdd06
Niroj Thapa <ngtc0925@gmail.com>
2023-12-27 00:19:42 +0545

    jwt validation and fetch data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController{
 
         @Post('signin')
         signin(@Body() dto:AuthDto ){
-            console.log(dto);
+            
             return this.authService.SignIn(dto);
         }
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Req,Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { userInfo } from 'os';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    @UseGuards(AuthGuard('jwt'))//for token validation
+    @Get('me')
+    getMe(@Req() req:Request){
+     console.log({
+        user:req.user
+     });
+     return req.user;
+    }
+}
