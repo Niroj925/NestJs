075a76b413f85fb3fc2280cd09a2d1299cb5f299
Niroj Thapa <ngtc0925@gmail.com>
2023-12-26 23:45:54 +0545

    passport js and jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,15 +4,23 @@ import *as argon from "argon2";
 import { AuthDto } from "./dto";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { error } from "console";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { promises } from "dns";
 
 @Injectable({})
 
 export class AuthService{
 
-    constructor(private prisma :PrismaService){}
+    constructor(
+        private prisma :PrismaService,
+        private jwt:JwtService,
+        private config:ConfigService
+        ){}
  
     async SignIn(dto: AuthDto) {
 
+        try{
         const user=await this .prisma.user.findUnique({
             where:{
                 email:dto.email
@@ -28,11 +36,19 @@ export class AuthService{
         if(!isCorrect){
             throw new ForbiddenException('Invalid credentials');
         }
-
-        delete user.hash
-
-        return user;
-       
+        return this.signToken(user.id,user.email);
+    }catch(error){
+        if(
+            error instanceof
+            PrismaClientKnownRequestError
+        ){
+            if(error.code === 'P2002'){
+                throw new ForbiddenException(
+                    'Invalid credentials'
+                )
+            }
+        }
+    }
     }
 
    async signUp(dto:AuthDto){
@@ -51,7 +67,7 @@ export class AuthService{
             //     createdAt:true
             // }
         });
-        
+
         delete user.hash
         return user;
     }catch(error){
@@ -68,4 +84,27 @@ export class AuthService{
         throw error;
     }
 }
+
+//to generate jwt token
+ async signToken(
+    userId:number,
+    email:string
+    ):Promise<{access_token: string}>{
+        const payload={
+            sub:userId,
+            email
+        }
+
+        const secret=this.config.get('JWT_SECRET');
+
+         const token=await this.jwt.signAsync(payload,{
+            expiresIn:"30m",
+            secret:secret,
+        })
+
+        return {
+            access_token:token
+        }
+}
+
 }
