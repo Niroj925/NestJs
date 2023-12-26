0cb26f8f727ef9ac5e527c58ee1f0618a464adba
Niroj Thapa <ngtc0925@gmail.com>
2023-12-27 00:23:56 +0545

    clean code for guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,16 +2,14 @@ import { Controller,Req,Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { userInfo } from 'os';
+import { jwtGuard } from 'src/auth/guard';
 
 @Controller('user')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt'))//for token validation
+    @UseGuards(jwtGuard)//for token validation
     @Get('me')
     getMe(@Req() req:Request){
-     console.log({
-        user:req.user
-     });
      return req.user;
     }
 }
